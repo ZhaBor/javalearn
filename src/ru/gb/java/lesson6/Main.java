36f82f32c15c5de0f7f923be269242fa3152fa74
Boris <zulu4@yandex.ru>
2020-06-16 21:12:24 +0500

    program for searching in file created

--- src/ru/gb/java/lesson6/Main.java
+++ src/ru/gb/java/lesson6/Main.java
@@ -7,11 +7,9 @@
 
 package ru.gb.java.lesson6;
 
-import java.io.File;
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
@@ -19,13 +17,19 @@ public class Main {
         String file1 = "c:/repo/javalearn/src/ru/gb/java/lesson6/1.txt";
         String file2 = "c:/repo/javalearn/src/ru/gb/java/lesson6/2.txt";
         String clued = "clued";
+        String toFind = "cat";
         clued =clued + concatenation(file1);
         clued =clued + concatenation(file2);
 
         System.out.println(clued);
-
-
+        if (search(file1,toFind)) {
+            System.out.printf("in file %s finded string %s \n", file1, toFind);
+        } else {
+            System.out.printf("in file %s string %s was not found \n", file1, toFind);
+        }
     }
+
+
     public static String concatenation(String file) {
         StringBuilder r = new StringBuilder();
         try {
@@ -41,6 +45,38 @@ public class Main {
         }
 
         return r.toString();
+    }
+
+    public static boolean search(String file, String toFind) {
+        char[] letters=new char[toFind.length()];
+        boolean flag = false;
+        int finded=0;
+        for(int i=0;i<toFind.length();i++) {
+            letters[i]=toFind.charAt(i);
+        }
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            int b;
+            while((b=fis.read())!=-1) {
+                if (((char)b == letters[0]) && (flag==false)) {
+                    flag=true;
+                    finded++;
+                    continue;
+                } else if (flag == true && (char)b == letters[finded]) {
+
+                    if ((finded+1)==toFind.length()) {
+                        return true;
+                    }
+                    finded++;
+                } else {
+                    flag=false;
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
 
